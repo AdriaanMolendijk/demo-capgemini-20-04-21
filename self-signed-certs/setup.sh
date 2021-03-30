e6bd0620a18518da75aee06e6e73d2d475f59b6c
Bryan van Lierop <bryanvlierop96@gmail.com>
2021-03-30 13:48:59 +0200

    Added commands self signed certs

--- self-signed-certs/setup.sh
+++ self-signed-certs/setup.sh
@@ -1,1 +1,6 @@
 k create -n openshift-config cm docker-cert --from-file=docker.apps.adriaanmolendijk.info=openshift.crt
+
+oc edit image.config.openshift.io/cluster
+# spec:
+#   additionalTrustedCA:
+#     name: docker-cert
