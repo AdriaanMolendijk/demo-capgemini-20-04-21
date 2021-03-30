1f82114216f185cbd960f16421ebc0f0c7fe688c
Bryan van Lierop <bryanvlierop96@gmail.com>
2021-03-30 14:22:51 +0200

    Edited self signed setup

--- self-signed-certs/setup.sh
+++ self-signed-certs/setup.sh
@@ -1,6 +1,12 @@
 k create -n openshift-config cm docker-cert --from-file=docker.apps.adriaanmolendijk.info=openshift.crt
 
 oc edit image.config.openshift.io/cluster
+
 # spec:
 #   additionalTrustedCA:
 #     name: docker-cert
+
+oc create secret docker-registry docker-creds \
+    --docker-server=docker.apps.adriaanmolendijk.info \
+    --docker-username=<username>  \
+    --docker-password=<password>
