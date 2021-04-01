a920d3f1c48d5672461c2bc5c848525d17a96905
Bryan van Lierop <bryanvlierop96@gmail.com>
2021-04-01 10:05:59 +0200

    Added compliance operator

---
+++ k8s/compliance-operator/setup.sh
@@ -0,0 +1,15 @@
+# get rules
+oc get rules.compliance
+
+# examples
+rhcos4-no-direct-root-logins
+rhcos4-no-empty-passwords
+
+# get profiles
+oc get profiles.compliance
+
+# get compliancescan
+oc get compliancescan
+
+# get results
+oc get compliancecheckresult
